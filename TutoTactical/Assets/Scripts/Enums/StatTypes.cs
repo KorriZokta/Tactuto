651e0f5aa3ea8f2b40321e72e30f9cbf5e5dbace
KorriZokta <frederic.becker4@gmail.com>
2017-08-21 11:38:45 +0200

    StatTypes -> Rank

---
+++ TutoTactical/Assets/Scripts/Enums/StatTypes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StatTypes {
+    LVL,    //Level
+    EXP,    //Experience
+    HP,     //Hit Points
+    MHP,    //Max HP
+    MP,     //Magic Points
+    MMP,    //Max MP
+    ATK,    //Physical Attack
+    DEF,    //Physical Defense
+    MAT,    //Magic Atk
+    MDF,    //Magic Def
+    EVD,    //Evade
+    RES,    //Status Resistance
+    SPD,    //Speed
+    MOV,    //Move Range
+    JMP,    //Jump Height
+    Count
+}
